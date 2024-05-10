1f1fb25aba32ac8eeac9a96186c6e47f82aaea9b
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-10 18:30:07 +0300

    serverless

--- functions/server.js
+++ functions/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { promisify } = require("util");
 const app = express();
-const port = 3000;
+// const port = 3000;
 const serverless = require('serverless-http');
 const router = express.Router();
 
@@ -91,6 +91,6 @@ app.use('/.netlify/functions/server', router);
 module.exports.handler = serverless(app);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening at http://localhost:${port}`);
+// });
