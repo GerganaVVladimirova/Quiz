4c4397ad89ca005536897dd0a28cffcb062b5538
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-10 16:48:33 +0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById('uploadForm').addEventListener('submit', async function(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  const formData = new FormData(this); // Get form data
+  const response = await fetch('/', { // Send form data to server
+      method: 'POST',
+      body: formData
+  });
+ 
+});
