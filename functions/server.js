1e133a2ef85a0f9b832f3d8988149829f3067cbd
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-10 20:03:59 +0300

    new

--- functions/server.js
+++ functions/server.js
@@ -69,7 +69,7 @@ router.post('/', upload, async (req, res) => {
 
   await unlinkAsync(req.file.path);
 
-  res.redirect('/quiz');
+  // res.redirect('/quiz');
 });
 
 router.get('/questions',(req,res)=>{
