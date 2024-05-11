24475a5c877dfbbccfbdd0c11720ee5e7fb14db8
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-11 09:48:59 +0300

    changes

--- dist/script.js
+++ dist/script.js
@@ -5,7 +5,22 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
   const formData = new FormData(this); // Get form data
   const response = await fetch('/upload', { // Send form data to server
       method: 'POST',
-      body: formData
+      body: formData,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
   });
-  window.location.href = "quiz.html";
+  // window.location.href = "quiz.html";
+  return redirect("/quiz");
 });
+
+
+// export async function action({ request }) {
+//   const form = await request.formData();
+
+//   await fetch("/", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//     body: form,
+//   });
+
+//   return redirect("/submitted");
+// }
