b906795c8bd687b3448d3e3616478c0927f75c32
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-11 09:09:38 +0300

    zx

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
   event.preventDefault(); // Prevent default form submission
 
   const formData = new FormData(this); // Get form data
-  const response = await fetch('/.netlify/functions/api/upload', { // Send form data to server
+  const response = await fetch('https://quiz-gvv.netlify.app/.netlify/functions/api/upload', { // Send form data to server
       method: 'POST',
       body: formData
   });
