b2777ec4c8b4faa239e9cce575b1ccf852078c50
Gergana Vladimirova <98710083+GerganaVVladimirova@users.noreply.github.com>
2024-05-11 08:52:25 +0300

    sx

--- dist/script.js
+++ dist/script.js
@@ -3,7 +3,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
   event.preventDefault(); // Prevent default form submission
 
   const formData = new FormData(this); // Get form data
-  const response = await fetch('/', { // Send form data to server
+  const response = await fetch('/.netlify/functions/api/upload', { // Send form data to server
       method: 'POST',
       body: formData
   });
